5941767c3da0292b57d84886606fdbe595257f4a
bj7 <jjbernitt@gmail.com>
2019-05-11 13:37:04 -0400

    added image assets to the asset folder and hooked up button and image views.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var randomDiceValue1: Int = 1
+    var randomDiceValue2: Int = 1
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func RollButtonClicked(_ sender: Any) {
+        randomDiceValue1 = Int.random(in: 1...6)
+        randomDiceValue2 = Int.random(in: 1...6)
 
+        print(randomDiceValue1, randomDiceValue2)
+    }
 }
 
